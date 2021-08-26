818a047d311022bb74e84000f87716315eb832d6
Zach Trocinski <ztrocinski@microsoft.com>
2021-08-26 14:49:56 -0500

    Updated parameter files and deployment script.

---
+++ bicep/0-demo-deploy.ps1
@@ -0,0 +1,28 @@
+$artifacts1 = $psscriptroot
+$rgname = 'bicepdemo'
+$region = 'CentralUS'
+Write-Warning -Message "Path is: [$artifacts1]"
+Write-Warning -Message "RG is: [$rgname] in Region: [$region]"
+break
+
+New-AzResourceGroup -Name $rgname -Location $region -Force
+
+# Create a layer above to orchestrate everything and provide features flags to use in Pipelines
+
+$MyParametersDeployALL = @{
+    ResourceGroupName     = $rgname
+    TemplateParameterFile = "$artifacts1\0-param-env1.json"
+    Verbose               = $true
+    WhatIf                = $false
+}
+
+# Orchestrate the deployment of all resources - VM and Storage or other
+#New-AzResourceGroupDeployment @MyParametersDeployALL -TemplateFile $artifacts1\ALL.bicep
+
+# Deploy Single layer, inner dev loop - VM only
+New-AzResourceGroupDeployment @MyParametersDeployALL -TemplateFile $artifacts1\VN.bicep
+
+# Deploy Single layer, inner dev loop - Storage only
+New-AzResourceGroupDeployment @MyParametersDeployALL -TemplateFile $artifacts1\WA.bicep
+
+Write-Warning -Message "Test"
