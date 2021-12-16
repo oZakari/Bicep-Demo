792cf1817e922d07e6f17aa75a4f4d7440f64b12
Zach Trocinski <ztrocinski@outlook.com>
2021-12-16 00:24:26 -0600

    Testing fix

--- release/Deploy.ps1
+++ release/Deploy.ps1
@@ -7,15 +7,29 @@ param (
 
 $Params = @{
     ResourceGroupName     = $RGName
-    TemplateParameterFile = ".\bicep\0-param-dev-env.json"
+    TemplateParameterFile = "$Artifacts\bicep\0-param-dev-env.json"
     Verbose               = $true
     WhatIf                = $false
+}
+
+Write-Warning -Message "Value of Artifacts is $Artifacts" -Verbose
+Write-Warning -Message "Value of RGName is $RGName" -Verbose
+Write-Warning -Message "Value of Region is $Region" -Verbose
+Write-Warning -Message "Value of Subscription is $Subscription" -Verbose
+Get-item -path $Artifacts -Verbose
+Get-item -path $TemplateFile -Verbose
+Get-item -path $TemplateParameterFile -Verbose
+
+$Params.getenumerator() | ForEach-Object {
+
+    Write-Verbose $_.Key -Verbose
+
+    Write-Warning $_.Value -Verbose
+
 }
 
 Set-AzContext -Subscription $Subscription
 
 New-AzResourceGroup -Name $RGName -Location $Region -Force
 
-Write-Warning -Message "Value of Artifacts is $Artifacts"
-
-New-AzResourceGroupDeployment @Params -TemplateFile ".\bicep\SA.bicep"
+New-AzResourceGroupDeployment @Params -TemplateFile "$Artifacts\bicep\SA.bicep"
